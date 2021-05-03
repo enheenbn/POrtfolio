c20a03185b11a51aa547cb74f44825e5197a747c
Your Name <you@example.com>
2021-05-03 15:42:49 +0800

    my portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+let nav = document.querySelector('.navbar');
+let val;
+
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+}
